b54e511d80541dc8f2b84535232ea41149b059a9
sthoms12 <137362252+sthoms12@users.noreply.github.com>
2024-12-31 22:48:41 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    // This is important for Cloudflare Pages
+    runtime: 'edge',
+  }
 }
 
 module.exports = nextConfig
